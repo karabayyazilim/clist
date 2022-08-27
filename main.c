5e7b91cd0b5d69969f113f3793be12351f2d3228
Gorkem95 <gorkem_095@hotmail.com>
2022-08-28 02:11:19 +0300

    Update

--- main.c
+++ main.c
@@ -1,26 +1,15 @@
 #include "clist.h"
 
-typedef struct s_data
-{
-	int x;
-	int y;
-} t_data;
-
 
 int main()
 {
 
-	c_list* list = list_create(sizeof(char));
-    
-	char str[] = "adsadasd";
+	c_list* list = list_create(sizeof(int));
 
-	for (int i = 0; i < 9; i++)
-		list_add(list, str[i]);
+	for (int i = 0; i < 10; i++)
+		list_add(list, i);
 
-	list_print(list);
-
-
-
-
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", *(int*)list_item_at(list, i));
 
 }
