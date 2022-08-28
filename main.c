30a57346c71a833604cfee0ef1dc3c974da57e97
Gorkem95 <gorkem_095@hotmail.com>
2022-08-28 10:32:49 +0300

    Update

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
 	c_list* list = list_create(sizeof(int));
 
 	for (int i = 0; i < 10; i++)
-		list_add(list, i + 1000000);
+		list_add(list, i);
 
 	for (int i = 0; i < 10; i++)
 		printf("%d\n", *(int*)list_item_at(list, i));
